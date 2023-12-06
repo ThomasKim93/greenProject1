e674933af317b00d6201a1f94d0d7ea8b827a234
unknown <yesolg90@gmail.com>
2023-12-06 14:45:29 +0900

    dd

--- app/component/Customer.js
+++ app/component/Customer.js
@@ -6,20 +6,22 @@ function Customer() {
   return (
     <>
     <section className={style.customer_section}>
-    <article>
+    <article className={style.customer1}>
       <h3>Customers</h3>
       <p>We are proud to serve a diverse range of businesses across various industries. Our clients come in all sizes, from startups to enterprises, and they all share a common need: to leverage the power of technology to achieve their goals.  </p>
       <div></div>
     </article>
 
+    <article className={style.customer2}>
     <ul>
       <li>
         <img src='../../../customers/mang.png'/>
-        <span>name</span>
+        <span>Sarah Jones</span>
         <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
-        <p className={style.star}>별점</p>
+        <div className={style.star}>별점</div>
       </li>
     </ul>
+    </article>
     </section>
       
     </>
--- app/page.js
+++ app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
     {/* <Aboutus/> */}
-    {/* <Customer/> */}
-    {/* <Blogs/> */}
-    <Solution/>
+    <Customer/>
+    {/* <Blogs/>
+    <Solution/> */}
     </>
   )
 }
