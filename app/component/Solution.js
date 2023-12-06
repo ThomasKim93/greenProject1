b531cdd6faacc1cf0d180c83eddb514d04d492f9
unknown <yesolg90@gmail.com>
2023-12-06 14:04:26 +0900

    다시

---
+++ app/component/Solution.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import style from '../pages/solution/solution.module.scss';
+
+function Solution() {
+    return (
+        <>
+            <section className={style.solution_section}>
+                <article className={style.solution1}>
+                    <h3>Explore Our Comprehensive Software Solutions</h3>
+                    <p>Navigate our extensive collection of software solutions, meticulously organized into categories that reflect the diverse needs of modern businesses</p>
+                </article>
+
+                <article className={style.solution2}>
+                    <nav>
+                        <a>All</a>
+                        <a>Cyber Security</a>
+                        <a>CRM</a>
+                        <a>Development</a>
+                    </nav>
+                </article>
+
+                <article className={style.soultion3}> 
+                    <div>
+                        <ul>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                            <li>
+                                <img src='../../../solution/Group 689.png'/>
+                                <span>Canvas</span>
+                                <p>For individuals, solopreneurs and freelance designers who want unlimited access to premium content, to create professional designs with ease and scale their productivity.</p>
+                                <a href='#'>Visit Website</a>
+                            </li>
+                        </ul>
+                    </div>
+                </article>
+            </section>
+        </>
+    )
+}
+
+export default Solution
--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@
 import Aboutus from './component/Aboutus'
 import Blogs from './component/Blogs'
 import Customer from './component/Customer'
+import Solution from './component/Solution'
 
 export default function Home() {
   return (
     <>
     {/* <Aboutus/> */}
     {/* <Customer/> */}
-    <Blogs/>
+    {/* <Blogs/> */}
+    <Solution/>
     </>
   )
 }
