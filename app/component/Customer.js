cc6535c9cec526752750801068d17e8ab2a91cbe
unknown <yesolg90@gmail.com>
2023-12-06 21:46:02 +0900

    customers css 작업

--- app/component/Customer.js
+++ app/component/Customer.js
@@ -5,25 +5,137 @@ import style from '../pages/customer/customer.module.scss';
 function Customer() {
   return (
     <>
-    <section className={style.customer_section}>
-    <article className={style.customer1}>
-      <h3>Customers</h3>
-      <p>We are proud to serve a diverse range of businesses across various industries. Our clients come in all sizes, from startups to enterprises, and they all share a common need: to leverage the power of technology to achieve their goals.  </p>
-      <div></div>
-    </article>
+      <section className={style.customer_section}>
+        <article className={style.customer1}>
+          <h3>Customers</h3>
+          <p>We are proud to serve a diverse range of businesses across various industries. Our clients come in all sizes, from startups to enterprises, and they all share a common need: to leverage the power of technology to achieve their goals.  </p>
+          <div></div>
+        </article>
 
-    <article className={style.customer2}>
-    <ul>
-      <li>
-        <img src='../../../customers/mang.png'/>
-        <span>Sarah Jones</span>
-        <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
-        <div className={style.star}>별점</div>
-      </li> 
-    </ul>
-    </article>
-    </section>
-      
+        <article className={style.customer2}>
+          <ul className={style.numbox}>
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num2}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num2}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num3}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num2}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num2}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num3}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num3}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+
+            <li className={style.num1}>
+              <img src='../../../customers/mang.png' />
+              <span>Sarah Jones</span>
+              <p>This platform is a lifesaver! It has taken the stress out of managing our software subscriptions.</p>
+              <div className={style.star}><img src='../../../customers/star.png' /></div>
+            </li>
+          </ul>
+        </article>
+      </section>
+
     </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -9,10 +9,10 @@ export default function Home() {
   return (
     <>
     {/* <Aboutus/> */}
-    {/* <Customer/> */}
+    <Customer/>
     {/* <Blogs/>
     <Solution/> */}
-    <Error/>
+    {/* <Error/> */}
     </>
   )
 }
